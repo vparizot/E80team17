f30f6095ffb9aee3c44c15f67f49c67a5bdabda1
Victoria Parizot <107150059+vparizot@users.noreply.github.com>
2024-03-19 16:14:34 -0700

    Update ZStateEstimator.h
    slope & intercept

--- libraries/main/ZStateEstimator.h
+++ libraries/main/ZStateEstimator.h
@@ -35,8 +35,8 @@ public:
 
 private:
   // set pressure sensor calibration slope and incercept below
-  const float depthCal_slope = 1;
-  const float depthCal_intercept = 1;
+  const float depthCal_slope = 0.0121;
+  const float depthCal_intercept = 1.24;
 
 };
 
